b100488561381b5375d3904c3ca3220561c43ab5
RaniyaRaseenm <raniyaraseenm@gmail.com>
2026-04-14 12:19:11 +0530

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> arr=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(arr.contains(nums[i])){
+                return true;
+            }
+            else
+            arr.add(nums[i]);
+        
+
+        }
+        return false;
+        
+     }
+}
